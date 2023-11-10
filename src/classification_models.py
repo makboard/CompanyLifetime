fec67e97c552302b9e35921680c4ba15a14fc300
Escape142 <escape756@gmail.com>
2023-11-10 14:15:19 +0000

    preliminary commit

---
+++ src/classification_models.py
@@ -0,0 +1,151 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import StratifiedKFold
+from sklearn import metrics
+import xgboost
+import numpy as np
+import pandas as pd
+
+from omegaconf import DictConfig
+import sys
+import os
+
+sys.path.append(os.getcwd())
+from src.pickle_manager import save_pickle
+
+
+def classification_metrics_print(
+    y_train: pd.DataFrame,
+    train_predictions: np.ndarray,
+    y_test: pd.DataFrame,
+    test_predictions: np.ndarray,
+    metrics_dict: dict,
+    key: str,
+) -> dict:
+    """Print classification metrics on train and test data"""
+    accuracy_train = metrics.accuracy_score(y_train, train_predictions)
+    precision_train = metrics.precision_score(
+        y_train, train_predictions, average="weighted"
+    )
+    recall_train = metrics.recall_score(y_train, train_predictions, average="weighted")
+    f1_train = metrics.f1_score(y_train, train_predictions, average="weighted")
+    print("Train \nAccuracy:", accuracy_train)
+    print("Precision:", precision_train)
+    print("Recall:", recall_train)
+    print("F1 Score:", f1_train)
+
+    accuracy_test = metrics.accuracy_score(y_test, test_predictions)
+    precision_test = metrics.precision_score(
+        y_test, test_predictions, average="weighted"
+    )
+    recall_test = metrics.recall_score(y_test, test_predictions, average="weighted")
+    f1_test = metrics.f1_score(y_test, test_predictions, average="weighted")
+    print("Test \nAccuracy:", accuracy_test)
+    print("Precision:", precision_test)
+    print("Recall:", recall_test)
+    print("F1 Score:", f1_test)
+
+    metrics_dict[key] = {"train": {}, "test": {}}
+    metrics_dict[key]["train"]["accuracy"] = accuracy_train
+    metrics_dict[key]["train"]["precision"] = precision_train
+    metrics_dict[key]["train"]["recall"] = recall_train
+    metrics_dict[key]["train"]["f1"] = f1_train
+
+    metrics_dict[key]["test"]["accuracy"] = accuracy_test
+    metrics_dict[key]["test"]["precision"] = precision_test
+    metrics_dict[key]["test"]["recall"] = recall_test
+    metrics_dict[key]["test"]["f1"] = f1_test
+
+    return metrics_dict
+
+
+def random_search_grid_cv(classifier, params, X, y):
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    grid_search = RandomizedSearchCV(
+        estimator=classifier,
+        param_distributions=params,
+        n_iter=1,
+        cv=skf,
+        verbose=2,
+        random_state=42,
+        n_jobs=-1,
+    )
+    grid_search.fit(X, y)
+    return grid_search.best_params_
+
+
+def logistic_regression_classification(
+    cfg: DictConfig,
+    X_train: pd.DataFrame,
+    y_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_test: pd.DataFrame,
+    metrics_dict: dict,
+) -> dict:
+    """Fit Logistic Regression for classification"""
+    print("Fitting Logistic Regression...")
+    classifier = LogisticRegression(**cfg.classifiers.log_reg.default_params)
+    if cfg.enable_optimization:
+        classifier = LogisticRegression(
+            **random_search_grid_cv(
+                classifier, cfg.classifiers.log_reg.grid_params, X_train, y_train
+            )
+        )
+    classifier.fit(X_train, y_train)
+    train_predictions = classifier.predict(X_train)
+    test_predictions = classifier.predict(X_test)
+
+    save_pickle(cfg.paths.models, cfg.files.log_reg_model, classifier)
+    key = "logistic_regression"
+    metrics_dict = classification_metrics_print(
+        y_train, train_predictions, y_test, test_predictions, metrics_dict, key
+    )
+    return metrics_dict
+
+
+def random_forest_classification(
+    cfg: DictConfig,
+    X_train: pd.DataFrame,
+    y_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_test: pd.DataFrame,
+    metrics_dict: dict,
+) -> dict:
+    """Fit Random Forest Classifier for classification"""
+    print("Fitting Random Forest Classifier...")
+    classifier = RandomForestClassifier(**cfg.classifiers.rf.default_params)
+    classifier.fit(X_train, y_train)
+    train_predictions = classifier.predict(X_train)
+    test_predictions = classifier.predict(X_test)
+
+    save_pickle(cfg.paths.models, cfg.files.rf_classifier_model, classifier)
+    key = "random_forest"
+    metrics_dict = classification_metrics_print(
+        y_train, train_predictions, y_test, test_predictions, metrics_dict, key
+    )
+    return metrics_dict
+
+
+def xgb_classification(
+    cfg: DictConfig,
+    X_train: pd.DataFrame,
+    y_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_test: pd.DataFrame,
+    metrics_dict: dict,
+) -> dict:
+    """Fit XGBoostClassifier for classification"""
+    print("Fitting XGBoost Classifier...")
+    classifier = xgboost.XGBClassifier(**cfg.classifiers.xgboost.default_params)
+    classifier.fit(X_train, y_train)
+    train_predictions = classifier.predict(X_train)
+    test_predictions = classifier.predict(X_test)
+
+    save_pickle(cfg.paths.models, cfg.files.xgb_classifier_model, classifier)
+    key = "xgb"
+    metrics_dict = classification_metrics_print(
+        y_train, train_predictions, y_test, test_predictions, metrics_dict, key
+    )
+    return metrics_dict
---
+++ train_classification.py
@@ -0,0 +1,159 @@
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+import pandas as pd
+
+import os
+import sys
+
+import hydra
+from omegaconf import DictConfig
+from typing import Literal
+
+import warnings
+
+from src.pickle_manager import save_pickle, open_parquet
+from src.classification_models import (
+    logistic_regression_classification,
+    multinomial_naive_bayes_classification,
+    random_forest_classification,
+    xgb_classification,
+)
+
+warnings.filterwarnings("ignore")
+
+sys.path.append(os.getcwd())
+
+
+def make_indices(X: pd.DataFrame) -> tuple[list, list, list]:
+    """Split features indices into binary, numerical and categorical"""
+    binary_columns = ["Тип субъекта", "Вновь созданный", "Наличие лицензий"]
+    binary_indices = np.array(
+        [(column in binary_columns) for column in X.columns], dtype=bool
+    )
+
+    categorical_columns = ["Основной вид деятельности", "Регион", "КатСубМСП"]
+    categorical_indices = np.array(
+        [(column in categorical_columns) for column in X.columns], dtype=bool
+    )
+    numeric_indices = ~(binary_indices + categorical_indices)
+    numerical_columns = X.columns[numeric_indices]
+
+    return binary_columns, categorical_columns, numerical_columns
+
+
+def preprocess_X(X: pd.DataFrame) -> pd.DataFrame:
+    """Perform scaling and encoding for particular columns"""
+    binary_columns, categorical_columns, numerical_columns = make_indices(X)
+
+    # Scale numerical
+    scaler = preprocessing.StandardScaler(with_mean=0)
+    X_num = X[numerical_columns]
+    X_num = scaler.fit_transform(X_num)
+    X_num = pd.DataFrame(X_num, columns=numerical_columns)
+
+    # Encode categorical
+    enc = preprocessing.OneHotEncoder(handle_unknown="ignore")
+    X_cat = X[categorical_columns]
+    X_cat = enc.fit_transform(X_cat)
+    X_cat = pd.DataFrame(
+        X_cat.toarray().astype(np.int8), columns=enc.get_feature_names_out()
+    )
+
+    # Concat all columns
+    X = pd.concat(
+        [
+            X[binary_columns].reset_index(drop=True),
+            X_num.reset_index(drop=True),
+            X_cat.reset_index(drop=True),
+        ],
+        axis=1,
+    )
+
+    return X
+
+def classify_lifetime(lifetime) -> Literal[0, 1, 2, 3, 4] | None:
+    if lifetime <= 12:
+        return 0
+    elif 12 < lifetime <= 24:
+        return 1
+    elif 24 < lifetime <= 48:
+        return 2
+    elif 48 < lifetime <= 120:
+        return 3
+    elif lifetime > 120:
+        return 4
+
+def train_test(
+    cfg: DictConfig,
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """Perform train/test split"""
+    print("Collecting data...")
+    data = open_parquet(cfg.paths.parquets, cfg.files.companies_feat)
+    print("Initial data shape:", data.shape)
+
+    data = data.dropna()
+    print("Final data shape:", data.shape)
+
+    data.drop(data.loc[data["Основной вид деятельности"] == "na"].index, inplace=True)
+    data.drop(data.loc[data["Основной вид деятельности"] == "No"].index, inplace=True)
+    data['lifetime'] = data['lifetime'].apply(classify_lifetime)
+    y = data["lifetime"].values
+    X = data.drop(["Наименование / ФИО", "ОГРН", "ИНН", "reg_date", "lifetime"], axis=1)
+
+    # Cut values in some columns
+    X["Основной вид деятельности"] = X.apply(
+        lambda row: row["Основной вид деятельности"][1:2]
+        if row["Основной вид деятельности"][0] == "0"
+        else row["Основной вид деятельности"][:2],
+        axis=1,
+    )
+    X["Регион"] = X.apply(
+        lambda row: row["Регион"][1:2]
+        if row["Регион"][0] == "0"
+        else row["Регион"][:2],
+        axis=1,
+    )
+
+    # Convert columns data types
+    X["Тип субъекта"] = (X["Тип субъекта"] == "Индивидуальный предприниматель").astype(
+        int
+    )
+    X[["Основной вид деятельности", "Регион"]] = X[
+        ["Основной вид деятельности", "Регион"]
+    ].astype(int)
+    X["Вновь созданный"] = (X["Вновь созданный"] == "Да").astype(int)
+    X["Наличие лицензий"] = (X["Наличие лицензий"] == "Да").astype(int)
+    X["КатСубМСП"] = X["КатСубМСП"].astype(int)
+
+    X = preprocess_X(X)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=123, stratify=y
+    )
+
+    return X_train, X_test, y_train, y_test
+
+
+@hydra.main(
+    version_base=None,
+    config_path=os.path.join(os.getcwd(), "config"),
+    config_name="classification_config.yaml",
+)
+def run_train(cfg: DictConfig):
+    metrics_dict = {}
+    X_train, X_test, y_train, y_test = train_test(cfg)
+    # metrics_dict = logistic_regression_classification(
+    #     cfg, X_train, y_train, X_test, y_test, metrics_dict
+    # )
+    metrics_dict = random_forest_classification(
+        cfg, X_train, y_train, X_test, y_test, metrics_dict
+    )
+    metrics_dict = xgb_classification(cfg, X_train, y_train, X_test, y_test, metrics_dict)
+    print(metrics_dict)
+    save_pickle(cfg.paths.models, cfg.files.metrics, metrics_dict)
+
+
+if __name__ == "__main__":
+    sys.argv.append("hydra.run.dir=out/${now:%Y-%m-%d}/${now:%H-%M-%S}")
+    run_train()
